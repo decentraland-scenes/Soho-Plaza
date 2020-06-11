af7b53c9ffa3c7a96123eca2dd7d54af2b11980e
nearnshaw <nearnshaw@decentraland.org>
2020-06-11 13:58:04 -0300

    message board

--- src/modules/messageboard.ts
+++ src/modules/messageboard.ts
@@ -4,56 +4,31 @@ import { CheckServer } from './serverHandler'
 let SFHeavyFont = new Font(Fonts.SanFrancisco_Heavy)
 
 export enum MessageBoards {
-  ARTICHOKE = 'artichoke',
-  TOWER = 'tower',
+  DISCO = 'disco',
 }
 
-/// ARTICHOKE
-const ArtichokeFloatingText = new Entity()
-export let ArtichokeFloatingTextShape = new TextShape('Write something')
-ArtichokeFloatingTextShape.color = Color3.FromHexString('#8040E2')
-ArtichokeFloatingTextShape.font = SFHeavyFont
-ArtichokeFloatingText.addComponent(ArtichokeFloatingTextShape)
-ArtichokeFloatingText.addComponent(
+// DISCO
+
+const DiscoFloatingText = new Entity()
+DiscoFloatingText.addComponent(
   new Transform({
-    position: new Vector3(51, 15.5, 31.7),
+    position: new Vector3(194.4, 9.75, 283.5),
     scale: new Vector3(1, 1, 1),
-    rotation: Quaternion.Euler(0, 180, 0),
+    rotation: Quaternion.Euler(0, 90, 0),
   })
 )
-engine.addEntity(ArtichokeFloatingText)
+engine.addEntity(DiscoFloatingText)
 
-ArtichokeFloatingText.getComponent(Transform).rotate(new Vector3(1, 0, 0), -15)
+let discoLetters: Entity[] = []
 
-// TOWER
-
-const TowerFloatingText = new Entity()
-TowerFloatingText.addComponent(
-  new Transform({
-    position: new Vector3(48.6, 21, 116.6),
-    scale: new Vector3(1, 1, 1),
-    rotation: Quaternion.Euler(0, 0, 0),
-  })
-)
-engine.addEntity(TowerFloatingText)
-
-const bkgModel = new Entity()
-
-bkgModel.addComponent(new GLTFShape('models/screen-moon.glb'))
-bkgModel.setParent(TowerFloatingText)
-
-engine.addEntity(bkgModel)
-
-let towerLetters: Entity[] = []
-
-let distanceMultiplier = 9
-let maxCharacters = 40
+let distanceMultiplier = 16.5
+let maxCharacters = 20
 for (let i = 0; i < maxCharacters; i++) {
-  let angle = 360 - (i * 360) / maxCharacters
-  let radAngle = angle * (Math.PI / 180)
+  let angle = (i * 45) / maxCharacters
+  let radAngle = angle * (Math.PI / 90) + 0.785398
 
   let letter = new Entity()
-  letter.setParent(TowerFloatingText)
+  letter.setParent(DiscoFloatingText)
   letter.addComponent(new TextShape(''))
   letter.getComponent(TextShape).fontSize = 22
   letter.getComponent(TextShape).color = Color3.White() //Color3.Blue()
@@ -65,48 +40,22 @@ for (let i = 0; i < maxCharacters; i++) {
         0,
         Math.cos(radAngle) * distanceMultiplier
       ),
-      rotation: Quaternion.Euler(0, angle - 180, 0),
+      rotation: Quaternion.Euler(0, angle - 270, 0),
     })
   )
   engine.addEntity(letter)
-  towerLetters.push(letter)
+  discoLetters.push(letter)
 }
 
-// sceneMessageBus.on('newText', x => {
-//   sceneState.bannerText = x.text
-//   FloatingTextShape.value = x.text
-// })
-
-let ArtichokeUIOpener = new Entity()
-ArtichokeUIOpener.addComponent(new GLTFShape('models/artichoke_message.glb')) // GLTFShape('models/Message.glb'))
-ArtichokeUIOpener.addComponent(
+let DiscoUIOpener = new Entity()
+DiscoUIOpener.addComponent(new GLTFShape('models/disco_message.glb'))
+DiscoUIOpener.addComponent(
   new Transform({
-    position: new Vector3(42, 8.4, 43),
+    position: new Vector3(182, 0.6, 291),
     scale: new Vector3(1, 1, 1),
   })
 )
-ArtichokeUIOpener.addComponent(
-  new OnPointerDown(
-    (e) => {
-      //openMessageBoardUI(ArtichokeUIOpener, MessageBoards.ARTICHOKE)
-    },
-    {
-      button: ActionButton.PRIMARY,
-      hoverText: 'Write something',
-    }
-  )
-)
-engine.addEntity(ArtichokeUIOpener)
-
-let TowerUIOpener = new Entity()
-TowerUIOpener.addComponent(new GLTFShape('models/message_booth.glb'))
-TowerUIOpener.addComponent(
-  new Transform({
-    position: new Vector3(43.8, 37.4, 122.4),
-    scale: new Vector3(1, 1, 1),
-  })
-)
-TowerUIOpener.addComponent(
+DiscoUIOpener.addComponent(
   new OnPointerDown(
     (e) => {
       //openMessageBoardUI(TowerUIOpener, MessageBoards.TOWER)
@@ -117,30 +66,23 @@ TowerUIOpener.addComponent(
     }
   )
 )
-engine.addEntity(TowerUIOpener)
+engine.addEntity(DiscoUIOpener)
 
-export function setTowerText(text: string) {
+export function setDiscoText(text: string) {
+  let shortenedText = text.substr(0, maxCharacters)
   for (let i = 0; i <= maxCharacters - 1; i++) {
-    let letter = towerLetters[i]
+    let letter = discoLetters[i]
     let textShape = letter.getComponent(TextShape)
 
     if (i < text.length) {
-      textShape.value = text[i]
+      textShape.value = shortenedText[i]
     } else {
       textShape.value = ''
     }
   }
 }
 
-class SpinTextSystem implements ISystem {
-  update(dt: number) {
-    const transform = TowerFloatingText.getComponent(Transform)
-
-    transform.rotate(Vector3.Up(), dt * 10)
-  }
-}
-
-engine.addSystem(new SpinTextSystem())
+setDiscoText('123456789012345678901234567890')
 
 //// FETCH CURRENT MESSAGES EN MESSAGE BOARDS
 //updateMessageBoards()
@@ -149,3 +91,41 @@ const messageRefreshInterval: number = 30
 // start system
 export let serverChecker = new CheckServer(messageRefreshInterval)
 engine.addSystem(serverChecker)
+
+/// SIMPLE
+// const ArtichokeFloatingText = new Entity()
+// export let ArtichokeFloatingTextShape = new TextShape('Write something')
+// ArtichokeFloatingTextShape.color = Color3.FromHexString('#8040E2')
+// ArtichokeFloatingTextShape.font = SFHeavyFont
+// ArtichokeFloatingText.addComponent(ArtichokeFloatingTextShape)
+// ArtichokeFloatingText.addComponent(
+//   new Transform({
+//     position: new Vector3(51, 15.5, 31.7),
+//     scale: new Vector3(1, 1, 1),
+//     rotation: Quaternion.Euler(0, 180, 0),
+//   })
+// )
+// engine.addEntity(ArtichokeFloatingText)
+
+// ArtichokeFloatingText.getComponent(Transform).rotate(new Vector3(1, 0, 0), -15)
+
+// let ArtichokeUIOpener = new Entity()
+// ArtichokeUIOpener.addComponent(new GLTFShape('models/artichoke_message.glb')) // GLTFShape('models/Message.glb'))
+// ArtichokeUIOpener.addComponent(
+//   new Transform({
+//     position: new Vector3(42, 8.4, 43),
+//     scale: new Vector3(1, 1, 1),
+//   })
+// )
+// ArtichokeUIOpener.addComponent(
+//   new OnPointerDown(
+//     (e) => {
+//       //openMessageBoardUI(ArtichokeUIOpener, MessageBoards.ARTICHOKE)
+//     },
+//     {
+//       button: ActionButton.PRIMARY,
+//       hoverText: 'Write something',
+//     }
+//   )
+// )
+// engine.addEntity(ArtichokeUIOpener)
