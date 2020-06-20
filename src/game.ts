43007fe23b3314422377498246d6320ae7b4e52a
Shibu <mshibuu@gmail.com>
2020-06-20 17:51:59 -0300

    update models

--- src/game.ts
+++ src/game.ts
@@ -113,6 +113,8 @@ lights_capsule.addComponent(
 )
 engine.addEntity(lights_capsule)
 
+//SKATEPARK AREA
+
 //add skate_park
 let skate_park = new Entity()
 skate_park.addComponent(new GLTFShape('models/skate_park.glb'))
@@ -123,3 +125,129 @@ skate_park.addComponent(
   })
 )
 engine.addEntity(skate_park)
+
+// VOXEL AREA
+
+//add kraken
+let kraken = new Entity()
+kraken.addComponent(new GLTFShape("models/kraken.glb"))
+kraken.addComponent(new Transform({
+	position: new Vector3(160, 0, 160),
+	rotation: Quaternion.Euler(0, 0, 0)
+}
+))
+engine.addEntity(kraken)
+
+//add enter_coin
+let enter_coin = new Entity()
+enter_coin.addComponent(new GLTFShape("models/enter_coin.glb"))
+enter_coin.addComponent(new Transform({
+	position: new Vector3(160, 0, 160),
+	rotation: Quaternion.Euler(0, 0, 0)
+}
+))
+engine.addEntity(enter_coin)
+
+//add chopper
+let chopper = new Entity()
+chopper.addComponent(new GLTFShape("models/chopper.glb"))
+chopper.addComponent(new Transform({
+	position: new Vector3(160, 0, 160),
+	rotation: Quaternion.Euler(0, 0, 0)
+}
+))
+engine.addEntity(chopper)
+
+// ADD NFT AREA
+
+//add nft_area
+let nft_area = new Entity()
+nft_area.addComponent(new GLTFShape("models/nft_area.glb"))
+nft_area.addComponent(new Transform({
+	position: new Vector3(160, 0, 160),
+	rotation: Quaternion.Euler(0, 0, 0)
+}
+))
+engine.addEntity(nft_area)
+
+//add ethremon_tenteink
+let ethremon_tenteink = new Entity()
+ethremon_tenteink.addComponent(new GLTFShape("models/ethremon_tenteink.glb"))
+ethremon_tenteink.addComponent(new Transform({
+	position: new Vector3(160, 0, 160),
+	rotation: Quaternion.Euler(0, 0, 0)
+}
+))
+engine.addEntity(ethremon_tenteink)
+
+//add ethremon_matara
+let ethremon_matara = new Entity()
+ethremon_matara.addComponent(new GLTFShape("models/ethremon_matara.glb"))
+ethremon_matara.addComponent(new Transform({
+	position: new Vector3(160, 0, 160),
+	rotation: Quaternion.Euler(0, 0, 0)
+}
+))
+engine.addEntity(ethremon_matara)
+
+//add kotaro
+let kotaro = new Entity()
+kotaro.addComponent(new GLTFShape("models/kotaro.glb"))
+kotaro.addComponent(new Transform({
+	position: new Vector3(160, 0, 160),
+	rotation: Quaternion.Euler(0, 0, 0)
+}
+))
+engine.addEntity(kotaro)
+
+//add water_fountain
+let water_fountain = new Entity()
+water_fountain.addComponent(new GLTFShape("models/water_fountain.glb"))
+water_fountain.addComponent(new Transform({
+	position: new Vector3(160, 0, 160),
+	rotation: Quaternion.Euler(0, 0, 0)
+}
+))
+engine.addEntity(water_fountain)
+
+// VOXEL FOUNTAIN 
+
+//add FirstRing
+let FirstRing = new Entity()
+FirstRing.addComponent(new GLTFShape("models/FirstRing.glb"))
+FirstRing.addComponent(new Transform({
+	position: new Vector3(160, 0, 160),
+	rotation: Quaternion.Euler(0, 0, 0)
+}
+))
+engine.addEntity(FirstRing)
+
+//add SecondRing
+let SecondRing = new Entity()
+SecondRing.addComponent(new GLTFShape("models/SecondRing.glb"))
+SecondRing.addComponent(new Transform({
+	position: new Vector3(160, 0, 160),
+	rotation: Quaternion.Euler(0, 0, 0)
+}
+))
+engine.addEntity(SecondRing)
+
+//add ThirdRing
+let ThirdRing = new Entity()
+ThirdRing.addComponent(new GLTFShape("models/ThirdRing.glb"))
+ThirdRing.addComponent(new Transform({
+	position: new Vector3(160, 0, 160),
+	rotation: Quaternion.Euler(0, 0, 0)
+}
+))
+engine.addEntity(ThirdRing)
+
+//add FourthRing
+let FourthRing = new Entity()
+FourthRing.addComponent(new GLTFShape("models/FourthRing.glb"))
+FourthRing.addComponent(new Transform({
+	position: new Vector3(160, 0, 160),
+	rotation: Quaternion.Euler(0, 0, 0)
+}
+))
+engine.addEntity(FourthRing)
