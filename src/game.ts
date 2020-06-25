5caf291b1b0605b1eba2cd32c549adb784262ab0
Tak-John Cheung <tak_john@hotmail.com>
2020-06-25 09:33:52 +0100

    updated voxel editor base grid voxel and aspect ratio fix for ui

--- src/game.ts
+++ src/game.ts
@@ -9,6 +9,7 @@ import { addLinks } from './modules/externalLinks'
 import { followingEye } from './modules/krakenEye'
 import { WearablesScanner } from './modules/scanner'
 import { Category } from './modules/wearables'
+import { addVoxels } from './voxels/game'
 
 Input.instance.subscribe('BUTTON_DOWN', ActionButton.PRIMARY, false, (e) => {
   log(`pos: `, Camera.instance.position)
@@ -23,20 +24,20 @@ addBuildings()
 
 // VIDEO SCREENS
 
-addScreens()
+// addScreens()
 
 // DANCE FLOOR
 
 // BOOM BOX
 
 //// Sequencer Fountain
-addZenquencer(
-  {
-    position: new Vector3(285.4, 0.2, 287.2),
-    rotation: Quaternion.Euler(0, 90, 0),
-  },
-  sceneMessageBus
-)
+// addZenquencer(
+//   {
+//     position: new Vector3(285.4, 0.2, 287.2),
+//     rotation: Quaternion.Euler(0, 90, 0),
+//   },
+//   sceneMessageBus
+// )
 
 //// Mural
 
@@ -44,24 +45,24 @@ addZenquencer(
 
 /// Voxel 3d editor
 
-//addVoxels()
+addVoxels()
 
 // Piano
 
-addPiano(
-  new Transform({
-    position: new Vector3(230, 0, 22),
-  }),
-  sceneMessageBus
-)
+// addPiano(
+//   new Transform({
+//     position: new Vector3(230, 0, 22),
+//   }),
+//   sceneMessageBus
+// )
 
 // Voxel fountain
 
-createFountain({ position: new Vector3(231.5, 0, 84.5) }, sceneMessageBus)
+// createFountain({ position: new Vector3(231.5, 0, 84.5) }, sceneMessageBus)
 
 // Links to partners
 
-addLinks()
+// addLinks()
 
 // Kraken following eye
 
