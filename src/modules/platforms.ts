85cec12cde997a5c18414837eb746b02aede47fe
nearnshaw <nearnshaw@decentraland.org>
2020-06-26 14:02:42 -0300

    trigger area for skate

--- src/modules/platforms.ts
+++ src/modules/platforms.ts
@@ -276,122 +276,32 @@ export function placePlatforms() {
     } else topDoor.toggle(true)
   })
 
-  //button
+  //add skate_01
+  let skate_01 = new Platform(
+    new GLTFShape('models/skate_01.glb'),
+    {
+      position: new Vector3(160, 0, 160),
+      rotation: Quaternion.Euler(0, 0, 0),
+    },
+    { position: new Vector3(142.3, 10, 215.3) },
+    new Vector3(0.75, 2, 1.5),
+    'Skate_Action',
+    'skate1'
+  )
 
-  // doors
-  //
-  //   // WHALE ELEVATOR
-  //   let whale_Elevator = new Platform(
-  //     new GLTFShape('models/TheWhale_Action_Elevator.glb'),
-  //     { rotation: Quaternion.Euler(0, 180, 0) },
-  //     { position: new Vector3(188.5, 3, 236) },
-  //     new Vector3(7, 3, 7),
-  //     'WhaleElevator_Action',
-  //     'whaleElevatorActivated'
-  //   )
-  //   //// MOON TOWER ELEVATOR
-  //   let moonTower_Elevator = new Platform(
-  //     new GLTFShape('models/MoonTower_Action_Elevator.glb'),
-  //     { rotation: Quaternion.Euler(0, 180, 0) },
-  //     { position: new Vector3(48.6, 2.4, 116.6) },
-  //     new Vector3(7, 3, 7),
-  //     'MoonTower_Action_Elevator',
-  //     'moonElevatorActivated'
-  //   )
-  //   ///////  SHELL ELEVATOR
-  //   let shell_elevator = new Platform(
-  //     new GLTFShape('models/shell_elevator.glb'),
-  //     { rotation: Quaternion.Euler(0, 180, 0) },
-  //     {
-  //       position: new Vector3(300.5, 11.3, 120),
-  //       rotation: Quaternion.Euler(0, 45, 0),
-  //     },
-  //     new Vector3(7, 2, 6),
-  //     'TheShell_ElevatorAction',
-  //     'shellElevatorActivated'
-  //   )
-  //   ///////  TRAIN ELEVATOR
-  //   let train_elevator = new Platform(
-  //     new GLTFShape('models/train_elevator.glb'),
-  //     { rotation: Quaternion.Euler(0, 180, 0) },
-  //     {
-  //       position: new Vector3(229.7, 1.3, 143),
-  //       rotation: Quaternion.Euler(0, 45, 0),
-  //     },
-  //     new Vector3(2, 2, 2),
-  //     'TrainElevator_Action',
-  //     'trainElevatorActivated'
-  //   )
-  //   //// BALOOON
-  //   let ballonIsFlying: boolean = false
-  //   let balloon = new Platform(
-  //     new GLTFShape('models/balloon.glb'),
-  //     { rotation: Quaternion.Euler(0, 180, 0) },
-  //     {
-  //       position: new Vector3(80, 2, 181),
-  //       rotation: Quaternion.Euler(0, 45, 0),
-  //     },
-  //     new Vector3(2, 1, 2),
-  //     'Balloon_Action',
-  //     'balloonActivated'
-  //   )
-  //   // TRAIN
-  //   let trainIsMoving: boolean = false
-  //   let train = new Platform(
-  //     new GLTFShape('models/train.glb'),
-  //     { rotation: Quaternion.Euler(0, 180, 0) },
-  //     {
-  //       position: new Vector3(234.5, 7, 143),
-  //       rotation: Quaternion.Euler(0, 45, 0),
-  //     },
-  //     new Vector3(2, 2, 6),
-  //     'Train_Action',
-  //     'trainActivated'
-  //   )
-  //   sceneMessageBus.on('artichokeElevatorActivated', (e) => {
-  //     artichoke_Elevator.activate()
-  //     log('artichoke elevator')
-  //   })
-  //   sceneMessageBus.on('whaleElevatorActivated', (e) => {
-  //     whale_Elevator.activate()
-  //     log('whale elevator')
-  //   })
-  //   sceneMessageBus.on('moonElevatorActivated', (e) => {
-  //     moonTower_Elevator.activate()
-  //     log('moon tower elevator')
-  //   })
-  //   sceneMessageBus.on('shellElevatorActivated', (e) => {
-  //     shell_elevator.activate()
-  //     log('shell elevator')
-  //   })
-  //   sceneMessageBus.on('trainElevatorActivated', (e) => {
-  //     train_elevator.activate()
-  //     log('train elevator')
-  //   })
-  //   sceneMessageBus.on('balloonActivated', (e) => {
-  //     if (ballonIsFlying) {
-  //       log('baloon was already in flight')
-  //       return
-  //     }
-  //     ballonIsFlying = true
-  //     balloon.activate()
-  //     balloon.addComponentOrReplace(
-  //       new utils.Delay(150 * 1000, () => {
-  //         ballonIsFlying = false
-  //       })
-  //     )
-  //   })
-  //   sceneMessageBus.on('trainActivated', (e) => {
-  //     if (trainIsMoving) {
-  //       log('train was already in movement')
-  //       return
-  //     }
-  //     trainIsMoving = true
-  //     train.activate()
-  //     train.addComponentOrReplace(
-  //       new utils.Delay(100 * 1000, () => {
-  //         trainIsMoving = false
-  //       })
-  //     )
-  //   })
+  let skate01IsMoving: boolean = false
+
+  sceneMessageBus.on('skate1', (e) => {
+    if (skate01IsMoving) {
+      log('skate was already moving')
+      return
+    }
+    skate01IsMoving = true
+    skate_01.activate()
+    skate_01.addComponentOrReplace(
+      new utils.Delay(10000, () => {
+        skate01IsMoving = false
+      })
+    )
+  })
 }
