b916cf60dc53932930a5793ebe66cd84aad7f421
Tak-John Cheung <tak_john@hotmail.com>
2020-06-27 10:37:58 +0100

    added nfts and placeholders for museum b

--- src/galleries/data.ts
+++ src/galleries/data.ts
@@ -1,15 +1,236 @@
 export const data = [
-  {
+  { // POAP Genesis Plaza
     code: 1,
+    contract_address: "0x22c1f6050e56d2876009903609a2cc3fef83b415",
+    token_id: "11930",
+    transform: {
+      position: { x: 160 - 118.873, y: 8.9, z: 160 - 8.845 },
+      rotation: { x: 0, y: 90 + 60, z: 0 },
+      scale: { x: 6, y: 6, z: 3 }
+    },
+    style: 18,
+    color: Color3.White(),
+    link: "https://opensea.io/assets/0x8dc3be37a1d83a174a4d417980bcb455e347ba65/11930"
+  },
+  { // POAP SuperRare
+    code: 2,
+    contract_address: "0x22c1f6050e56d2876009903609a2cc3fef83b415",
+    token_id: "12129",
+    transform: {
+      position: { x: 160 - 134.691, y: 8.9, z: 160 - 9.033 },
+      rotation: { x: 0, y: 90 + 120, z: 0 },
+      scale: { x: 6, y: 6, z: 3 }
+    },
+    style: 18,
+    color: Color3.White(),
+    link: "https://opensea.io/assets/0x22c1f6050e56d2876009903609a2cc3fef83b415/12129"
+  },
+  { // POAP OpenSea
+    code: 3,
+    contract_address: "0x22c1f6050e56d2876009903609a2cc3fef83b415",
+    token_id: "12385",
+    transform: {
+      position: { x: 160 - 142.763, y: 8.9, z: 160 + 4.571 },
+      rotation: { x: 0, y: 90 + 180, z: 0 },
+      scale: { x: 6, y: 6, z: 3 }
+    },
+    style: 18,
+    color: Color3.White(),
+    link: "https://opensea.io/assets/0x22c1f6050e56d2876009903609a2cc3fef83b415/12129"
+  },
+  { // POAP Mintbase
+    code: 4,
+    contract_address: "0x22c1f6050e56d2876009903609a2cc3fef83b415",
+    token_id: "12564",
+    transform: {
+      position: { x: 160 - 135.017, y: 8.9, z: 160 + 18.364 },
+      rotation: { x: 0, y: 90 + 240, z: 0 },
+      scale: { x: 6, y: 6, z: 3 }
+    },
+    style: 18,
+    color: Color3.White(),
+    link: "https://opensea.io/assets/0x22c1f6050e56d2876009903609a2cc3fef83b415/12564"
+  },
+  { // POAP Abner Harris
+    code: 5,
+    contract_address: "0x22c1f6050e56d2876009903609a2cc3fef83b415",
+    token_id: "12751",
+    transform: {
+      position: { x: 160 - 119.199, y: 8.9, z: 160 + 18.552 },
+      rotation: { x: 0, y: 90 - 60, z: 0 },
+      scale: { x: 6, y: 6, z: 3 }
+    },
+    style: 18,
+    color: Color3.White(),
+    link: "https://opensea.io/assets/0x22c1f6050e56d2876009903609a2cc3fef83b415/12751"
+  },
+  { // POAP PixelChain
+    code: 6,
+    contract_address: "0x22c1f6050e56d2876009903609a2cc3fef83b415",
+    token_id: "12903",
+    transform: {
+      position: { x: 160 - 111.127, y: 8.9, z: 160 + 4.947 },
+      rotation: { x: 0, y: 90, z: 0 },
+      scale: { x: 6, y: 6, z: 3 }
+    },
+    style: 18,
+    color: Color3.White(),
+    link: "https://opensea.io/assets/0x22c1f6050e56d2876009903609a2cc3fef83b415/12903"
+  },
+  { // POAP ChainGuardians 
+    code: 7,
+    contract_address: "0x22c1f6050e56d2876009903609a2cc3fef83b415",
+    token_id: "13054",
+    transform: {
+      position: { x: 160 - 117.83, y: 15.271, z: 160 - 10.604 },
+      rotation: { x: 0, y: 90 + 60, z: 0 },
+      scale: { x: 5, y: 5, z: 2.5 }
+    },
+    style: 18,
+    color: Color3.White(),
+    link: "https://opensea.io/assets/0x22c1f6050e56d2876009903609a2cc3fef83b415/13054"
+  },
+  { // POAP MakersPlace
+    code: 8,
+    contract_address: "0x22c1f6050e56d2876009903609a2cc3fef83b415",
+    token_id: "13232",
+    transform: {
+      position: { x: 160 - 126.733, y: 15.271, z: 160 - 13.104 },
+      rotation: { x: 0, y: 90 + 90, z: 0 },
+      scale: { x: 5, y: 5, z: 2.5 }
+    },
+    style: 18,
+    color: Color3.White(),
+    link: "https://opensea.io/assets/0x22c1f6050e56d2876009903609a2cc3fef83b415/13232"
+  },
+  { // POAP Known Origin (REPLACE)
+    code: 9,
+    contract_address: "0x22c1f6050e56d2876009903609a2cc3fef83b415",
+    token_id: "13232",
+    transform: {
+      position: { x: 160 - 135.693, y: 15.271, z: 160 - 10.817 },
+      rotation: { x: 0, y: 90 + 120, z: 0 },
+      scale: { x: 5, y: 5, z: 2.5 }
+    },
+    style: 18,
+    color: Color3.White(),
+    link: "https://opensea.io/assets/0x22c1f6050e56d2876009903609a2cc3fef83b415/13232"
+  },
+  { // POAP Soho (REPLACE)
+    code: 10,
+    contract_address: "0x22c1f6050e56d2876009903609a2cc3fef83b415",
+    token_id: "13232",
+    transform: {
+      position: { x: 160 - 142.309, y: 15.271, z: 160 - 4.356 },
+      rotation: { x: 0, y: 90 + 150, z: 0 },
+      scale: { x: 5, y: 5, z: 2.5 }
+    },
+    style: 18,
+    color: Color3.White(),
+    link: "https://opensea.io/assets/0x22c1f6050e56d2876009903609a2cc3fef83b415/13232"
+  },
+  { // POAP Async (REPLACE)
+    code: 11,
+    contract_address: "0x22c1f6050e56d2876009903609a2cc3fef83b415",
+    token_id: "13232",
+    transform: {
+      position: { x: 160 - 144.808, y: 15.271, z: 160 + 4.547 },
+      rotation: { x: 0, y: 90 + 180, z: 0 },
+      scale: { x: 5, y: 5, z: 2.5 }
+    },
+    style: 18,
+    color: Color3.White(),
+    link: "https://opensea.io/assets/0x22c1f6050e56d2876009903609a2cc3fef83b415/13232"
+  },
+  { // CryptoMorph Decentraland
+    code: 12,
     contract_address: "0x8dc3be37a1d83a174a4d417980bcb455e347ba65",
     token_id: "40",
     transform: {
-      position: { x: 74.619, y: 3.17, z: 160 + 126.427 },
-      rotation: { x: 0, y: -72.25, z: 0 },
-      scale: { x: 4, y: 4, z: 2 }
+      position: { x: 160 - 142.521, y: 15.271, z: 160 + 13.507 },
+      rotation: { x: 0, y: 90 + 210, z: 0 },
+      scale: { x: 5, y: 5, z: 2.5 }
     },
     style: 18,
     color: Color3.White(),
     link: "https://opensea.io/assets/0x8dc3be37a1d83a174a4d417980bcb455e347ba65/40"
+  },
+  { // PixelChain Decentraland
+    code: 13,
+    contract_address: "0xbc0e164ee423b7800e355b012c06446e28b1a29d",
+    token_id: "1021",
+    transform: {
+      position: { x: 160 - 136.06, y: 15.271, z: 160 + 20.123 },
+      rotation: { x: 0, y: 90 + 240, z: 0 },
+      scale: { x: 5, y: 5, z: 2.5 }
+    },
+    style: 18,
+    color: Color3.White(),
+    link: "https://opensea.io/assets/0xbc0e164ee423b7800e355b012c06446e28b1a29d/1021"
+  },
+  { // Girl in Spy Suit and Ice Cream Hat
+    code: 14,
+    contract_address: "0x4da54e1fa95b7f28fb9ff1df06c89070c1ab0b03",
+    token_id: "7",
+    transform: {
+      position: { x: 160 - 127.157, y: 15.271, z: 160 + 22.623 },
+      rotation: { x: 0, y: 0, z: 0 },
+      scale: { x: 5, y: 5, z: 2.5 }
+    },
+    style: 18,
+    color: Color3.White(),
+    link: "https://opensea.io/assets/0x4da54e1fa95b7f28fb9ff1df06c89070c1ab0b03/7"
+  },
+  { // DCL - Open the Gates
+    code: 15,
+    contract_address: "0xfbeef911dc5821886e1dda71586d90ed28174b7d",
+    token_id: "136972",
+    transform: {
+      position: { x: 160 - 118.197, y: 15.271, z: 160 + 20.336 },
+      rotation: { x: 0, y: 90 - 60, z: 0 },
+      scale: { x: 5, y: 5, z: 2.5 }
+    },
+    style: 18,
+    color: Color3.White(),
+    link: "https://opensea.io/assets/0xfbeef911dc5821886e1dda71586d90ed28174b7d/136972"
+  },
+  { // MANA-FEVER.FUN​
+    code: 16,
+    contract_address: "0xbc0e164ee423b7800e355b012c06446e28b1a29d",
+    token_id: "2546",
+    transform: {
+      position: { x: 160 - 111.581, y: 15.271, z: 160 + 13.875 },
+      rotation: { x: 0, y: 90 - 30, z: 0 },
+      scale: { x: 5, y: 5, z: 2.5 }
+    },
+    style: 18,
+    color: Color3.White(),
+    link: "https://opensea.io/assets/0xbc0e164ee423b7800e355b012c06446e28b1a29d/2546"
+  },
+  { // Tune In #12
+    code: 17,
+    contract_address: "0xecf7ef42b57ee37a959bf507183c5dd6bf182081",
+    token_id: "13",
+    transform: {
+      position: { x: 160 - 109.082, y: 15.271, z: 160 + 4.972 },
+      rotation: { x: 0, y: 0 + 90, z: 0 },
+      scale: { x: 5, y: 5, z: 2.5 }
+    },
+    style: 18,
+    color: Color3.White(),
+    link: "https://opensea.io/assets/0xecf7ef42b57ee37a959bf507183c5dd6bf182081/13"
+  },
+  { // Shibu's Masterpiece (REPLACE)
+    code: 18,
+    contract_address: "0xecf7ef42b57ee37a959bf507183c5dd6bf182081",
+    token_id: "13",
+    transform: {
+      position: { x: 160 - 111.369, y: 15.271, z: 160 - 3.988 },
+      rotation: { x: 0, y: 90 + 30, z: 0 },
+      scale: { x: 5, y: 5, z: 2.5 }
+    },
+    style: 18,
+    color: Color3.White(),
+    link: "https://opensea.io/assets/0xecf7ef42b57ee37a959bf507183c5dd6bf182081/13"
   },
 ]
