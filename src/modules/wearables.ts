d5f7a3aca66e9903fe71e181cd6b5d68415a7c9f
nearnshaw <nearnshaw@decentraland.org>
2020-06-28 15:36:52 -0300

    fix wearables scanner

--- src/modules/wearables.ts
+++ src/modules/wearables.ts
@@ -1,27 +1,26 @@
-// import * as cryptoUtils from '../../node_modules/decentraland-crypto-utils/avatar/index'
-// import * as wearables from '../../node_modules/decentraland-crypto-utils/wearable/index'
+import * as cryptoUtils from '../../node_modules/decentraland-crypto-utils/avatar/index'
 
 export async function checkWearableCategory(category: Category) {
-  //   let wearablesList = await wearables.getListOfWearables()
-  //   let equipped = await cryptoUtils.equipedItems()
+  let wearablesList = await getListOfWearables()
+  let equipped = await cryptoUtils.equipedItems()
 
-  //   log(wearablesList, equipped)
+  log(wearablesList, equipped)
 
-  //   for (let item of equipped) {
-  //     for (let wearablesCollection of wearablesList) {
-  //       for (let wearable of wearablesCollection.wearables) {
-  //         if (item === wearable.id) {
-  //           if (wearable.category == category) {
-  //             log('found matching wearable! ', item)
-  //             return true
-  //           } else {
-  //             continue
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
-  //   log('no matching wearables')
+  for (let item of equipped) {
+    for (let wearablesCollection of wearablesList) {
+      for (let wearable of wearablesCollection.wearables) {
+        if (item === wearable.id) {
+          if (wearable.category == category) {
+            log('found matching wearable! ', item)
+            return true
+          } else {
+            continue
+          }
+        }
+      }
+    }
+  }
+  log('no matching wearables')
   return false
 }
 
@@ -52,4 +51,64 @@ export enum Rarity {
   Mythic = 'mythic',
   Rare = 'rare',
   Uncommon = 'uncommon',
+}
+
+export interface Wearables {
+  id: string
+  wearables: Wearable[]
+}
+
+export interface Wearable {
+  id: string
+  representations: Representation[]
+  type: Type
+  category: Category
+  tags: string[]
+  baseUrl: string
+  i18n: I18N[]
+  thumbnail: string
+  image: string
+  replaces?: Category[]
+  hides?: Category[]
+  description?: string
+  rarity?: Rarity
+}
+
+export interface I18N {
+  code: Code
+  text: string
+}
+
+export enum Code {
+  En = 'en',
+  Es = 'es',
+}
+
+export interface Representation {
+  bodyShapes: BodyShape[]
+  mainFile: string
+  contents: Content[]
+  overrideReplaces?: any[]
+  overrideHides?: any[]
+}
+
+export enum BodyShape {
+  Basefemale = 'Basefemale',
+  DCLBaseAvatarsBaseFemale = 'dcl://base-avatars/BaseFemale',
+  DCLBaseAvatarsBaseMale = 'dcl://base-avatars/BaseMale',
+}
+
+export interface Content {
+  file: string
+  hash: string
+}
+
+export enum Type {
+  Wearable = 'wearable',
+}
+
+export async function getListOfWearables() {
+  return (await fetch(
+    'https://wearable-api.decentraland.org/v2/collections'
+  ).then((res) => res.json())) as Wearables[]
 }
